3f75cdd4a68da85a128a8db965ca6729c8acb25c
Shujian <shujian.bu@gmail.com>
2016-03-09 23:01:50 -0600

    fix: fix credentials for travis deployment

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -6,6 +6,8 @@ mkdir out;
 cd out
 cp -r ../build/* .
 git init
+git config user.name "Shujian Bu"
+git config user.email "shujian.bu@gmail.com"
 git add .
 git commit -m "auto deployment to gh-pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
